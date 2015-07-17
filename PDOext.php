fa3e64c53045922b907ab9d2cc637d2fd354d9c2
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-07-17 09:58:58 +0200

    __construct(): Specify connection parameters as array

--- PDOext.php
+++ PDOext.php
@@ -1,6 +1,45 @@
 <?php
 class PDOext extends PDO {
   function __construct($dsn, $username=null, $password=null, $options=array()) {
-    parent::__construct($dsn, $username, $password, $options);
+    if(is_array($dsn)) {
+      $_dsn = array();
+
+      if(!array_key_exists('type', $dsn)) {
+	trigger_error("PDOext::__construct(): type not specified", E_USER_ERROR);
+      }
+
+      if($dsn['type'] == "sqlite") {
+	if(array_key_exists('path', $dsn))
+	  $_dsn[] = $dsn['path'];
+      }
+
+      if(array_key_exists('dbname', $dsn))
+	$_dsn[] = "dbname={$dsn['dbname']}";
+
+      if(array_key_exists('host', $dsn))
+	$_dsn[] = "host={$dsn['host']}";
+
+      if(array_key_exists('host', $dsn))
+	$_dsn[] = "host={$dsn['host']}";
+
+      if(array_key_exists('unix_socket', $dsn))
+	$_dsn[] = "unix_socket={$dsn['unix_socket']}";
+
+      if(array_key_exists('charset', $dsn))
+	$_dsn[] = "charset={$dsn['charset']}";
+
+      $_dsn = "{$dsn['type']}:" . implode(";", $_dsn);
+
+      if(!array_key_exists('username', $_dsn))
+	$dsn['username'] = null;
+
+      if(!array_key_exists('password', $_dsn))
+	$dsn['password'] = null;
+
+      parent::__construct($_dsn, $dsn['username'], $dsn['password']);
+    }
+    else {
+      parent::__construct($dsn, $username, $password, $options);
+    }
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# PDOext
+PDOext
+======
 PDO (PHP Database Objects) extended - adds a few missing functions
+
+Functions
+=========
+__construct($connection, $options)
+---------------------
+Alternatively to specifying a DSN, username and password as parameters you can specify connection parameters as array:
+
+```php
+array(
+  'type' => 'mysql',
+  'dbname' => 'database',
+)
+```
+
+Optional connection parameters:
+```php
+  'username' => 'USER',
+  'password' => 'PASSWORD',
+  'host' => 'localhost',
+  'port' => 3333, // MYSQL, PGSQL
+  'unix_socket' => '/path/to/socket', // MYSQL
+  'path' => '/path/to/db', // SQLite
+```
